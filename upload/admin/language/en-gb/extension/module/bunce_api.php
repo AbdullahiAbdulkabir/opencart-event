a2b4c618f0836c763ad2034fd5136af47833099a
surecoder <gatukurh1@gmail.com>
2024-10-25 12:57:29 +0100

    Initial commit

---
+++ upload/admin/language/en-gb/extension/module/bunce_api.php
@@ -0,0 +1,28 @@
+<?php
+// Heading
+$_['heading_title']    = 'Bunce API Integration';
+
+// Text
+$_['text_extension']   = 'Extensions';
+$_['text_success']     = 'Success: You have modified Bunce API integration!';
+$_['text_edit']        = 'Edit Bunce API Integration';
+$_['text_home']        = 'Home';
+$_['text_checkout_event'] = 'Checkout Event';
+$_['text_view_product_event'] = 'View Product Event';
+$_['text_add_to_cart_event'] = 'Add to Cart Event';
+$_['text_abandoned_cart_event'] = 'Abandoned Cart Event';
+
+// Entry
+$_['entry_event_id_checkout']   = 'Event ID for Checkout';
+$_['entry_event_id_view_product'] = 'Event ID for Product View';
+$_['entry_event_id_add_to_cart'] = 'Event ID for Add to Cart';
+$_['entry_event_id_abandoned_cart'] = 'Event ID for Abandoned Cart';
+$_['entry_abandoned_cart_duration'] = 'Abandoned Cart Duration (minutes)';
+$_['entry_api_key']    = 'API Key';
+$_['entry_status']     = 'Status';
+
+// Error
+$_['error_event_id']     = 'Event ID required!';
+$_['error_abandoned_cart_duration'] = 'Abandoned Cart Duration required!';
+$_['error_api_key']      = 'API Key required!';
+$_['error_permission'] = 'Warning: You do not have permission to modify Bunce API integration!';
